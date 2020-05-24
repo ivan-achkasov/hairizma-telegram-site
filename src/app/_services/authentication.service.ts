ab3bc459bc23a1d8fb4cafe69c717765b8072664
ivan <inset -4px 0px 8px -8px>
2020-05-24 19:57:20 +0300

    Implemented aside block with menu

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from "rxjs";
 import { User } from "../_models/user";
-import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
